de073e8a2292740567d5f3db78b3907ae43cc253
Ben <benjaminfuller98@gmail.com>
2019-05-05 12:59:55 -0600

    ready to merge with turn enabled

--- index.hs
+++ index.hs
@@ -31,8 +31,8 @@ create = base "W"
                       : Rook {color = c}
                       : []
 
-move :: [[Piece]] -> (Int, Int) -> (Int, Int) -> [[Piece]]
-move board (x0, y0) (x1, y1) = if (validate (board !! x0 !! y0) (x0, y0) (x1, y1) board)
+move :: String -> [[Piece]] -> (Int, Int) -> (Int, Int) -> [[Piece]]
+move turn board (x0, y0) (x1, y1) = if (validate (board !! x0 !! y0) (x0, y0) (x1, y1) board) && ((getCell board (x0,y0)) == turn)
   then
     map (\r ->
       map
@@ -155,13 +155,13 @@ isPathClearDiagonal board (x0, y0) (x1, y1) =
         else if(x0 < x1) && (y0 > y1) then if ((getCell board ((x0 + 1), (y0 - 1))) == "Empty") || (((x0 + 1) == x1) && ((y0 - 1) == y1)) then isPathClearDiagonal board (x0 + 1, y0 - 1) (x1, y1) else False
         else if ((getCell board ((x0 + 1), (y0 + 1))) == "Empty") || (((x0 + 1) == x1) && (y1 == (y0 + 1))) then isPathClearDiagonal board (x0 + 1, y0 + 1) (x1, y1) else False
 
-inCheck :: [[Piece]] -> Bool
-inCheck board = foldl (\acc (index, piece) -> acc || validate piece (div index 8, mod index 8) (div king 8, mod king 8) board) False (zip [0..] (concat board))
+inCheck :: String -> [[Piece]] -> Bool
+inCheck turn board = foldl (\acc (index, piece) -> acc || validate piece (div index 8, mod index 8) (div king 8, mod king 8) board) False (zip [0..] (concat board))
   where king = fst $ (filter (\(index, cell) -> cell == King {color = "W"}) $ zip [0..] (concat board)) !! 0
 
-inCheckMate :: [[Piece]] -> Bool
-inCheckMate board =
-  inCheck board
+inCheckMate :: String -> [[Piece]] -> Bool
+inCheckMate turn board =
+  inCheck turn board
   &&
   foldl
     (\acc (index, piece) ->
@@ -170,10 +170,10 @@ inCheckMate board =
       foldl
         (&&)
         True
-        (map (\cell -> inCheck $ move board (div index 8, mod index 8) (div cell 8, mod cell 8)) [0,1..63])
+        (map (\cell -> inCheck turn $ move turn board (div index 8, mod index 8) (div cell 8, mod cell 8)) [0,1..63])
     )
     True
-    (filter (\(index, piece) -> getColor piece == "W") (zip [0..] (concat board)))
+    (filter (\(index, piece) -> getColor piece == changeColor turn) (zip [0..] (concat board)))
 
 
 
@@ -209,22 +209,25 @@ validateInput prompt = do
     Just parsed -> return parsed
     Nothing -> putStrLn "Invalid input. Please enter your position as (row, column)." >> validateInput prompt
 
-play :: Int -> [[Piece]] -> IO ()
-play (-1) board = do
+changeColor :: String -> String
+changeColor color = if color == "B" then "W" else "B"
+
+play :: String -> Int -> [[Piece]] -> IO ()
+play turn (-1) board = do
   putStr("\nGood game!\n")
-play step board = do
+play turn step board = do
   putStr $ draw $ board
-  if inCheckMate board
+  if inCheckMate turn board
     then do
       putStr "\nYou are finished!"
-      play (-1) board
-    else if inCheck board
+      play "B" (-1) board
+    else if inCheck turn board
       then putStr "\nYou are in check!"
       else do
         putStr "\nPlay wisely!"
         initial <- validateInput "\n(r0, c0):"
         final <- validateInput "\n(r1, c1):"
         putStr "\n"
-        play (step + 1) $ move board initial final
+        play (changeColor turn) (step + 1) $ move turn board initial final
 
-main = do play 0 $ create
+main = do play "W" 0 $ create
